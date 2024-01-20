966e2889ef87df185a0fcabd33153a6056e9e84f
zaham <zaham2@yahoo.com>
2024-01-20 16:30:01 +0200

    Made controllers and initial schema

--- backend/server.js
+++ backend/server.js
@@ -4,15 +4,9 @@ const app = express()
 const PORT = process.env.PORT || 3005;
 
 app.get('/', async (req, res) => {
-    const test = await prisma.test.create({
-        data: {
-            bio: 'someone\'s bio should go here',
-            userId: 1
-        }
-    })
 
-    console.log(test)
-    res.send(test)
 })
+
+
 
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`))
